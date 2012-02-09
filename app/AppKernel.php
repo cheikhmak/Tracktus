b605ff21512d79feb3da36e8d129e5ea7256c606
Pierre GOUDJO <pierre.goudjo@gmail.com>
2012-02-09 15:48:05 +0100

    Cretion of TracktusAppBundle which will contains all the app-relative stuff

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Tracktus\UserBundle\TracktusUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Tracktus\AppBundle\TracktusAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
